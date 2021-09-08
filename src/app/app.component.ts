f40a49cdc5d0575bca50447f1d06e07ad44c3881
Nikitaben <nikitaprajapati88@gmail.com>
2021-09-08 11:53:16 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'hello-ng',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  allItems = [
+    {description: 'Item 1', isDone: true},
+    {description: 'Item 2', isDone: false},
+    {description: 'Item 3', isDone: true},
+    {description: 'Item 4', isDone: false},
+  ]
   title = 'to-do-list';
+
+  addItem(description: string){
+    this.allItems.unshift({ description: 'Item 1', isDone: false })
+  }
 }
